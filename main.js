552c8d4fac703b7482445759d463be30a6f2a957
Leticia <lsf@protonmail.ch>
2020-05-11 17:26:09 +0200

    messaggio con orario

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 // Milestone 1.2
 //Aggiunta di un messaggio​: l’utente scrive un testo nella parte bassa e cliccando “invia” il testo viene aggiunto al thread sopra, come messaggio verde
 
-$('.fas.fa-microphone').click(function(){
+$('.fa-paper-plane').click(function(){
+    // debugger;
     //intercetto il messaggio dell'utente
     var msgUser = $('#send-msg').val();
     console.log(msgUser);
+    //primo stampo il messaggio dell'utente con text --che sostituisce tutto gia scritto prima
+    $('.template .chat-with .chat-me p:first-child').text(msgUser);
     //copio l'elemento template
     var msgTemplate = $('.template .chat-with .chat-me').clone();
     console.log(msgTemplate);
-    //aggiungo il testo dell'utente
-    msgTemplate.text(msgUser);
-    //aggiungere la chat
-    $('.chat-with').append(msgTemplate)
+    //aggiungere il messaggio alla chat
+    $('.chat-with.real').append(msgTemplate)
+});
+
+//cambio icona quando clicco dentro l'input per inviare il messaggio
+
+$('#send-msg').click(function(){
+    //nascondo l'icona presente -- il microfono
+    $('.fas.fa-microphone').hide();
+    //faccio apparire l'altra icona
+    $('fa-paper-plane').show();
 });
