c00c0ffcef05b0862953d0c3df8df8745ac16d53
Leticia <lsf@protonmail.ch>
2020-05-12 21:27:08 +0200

    invio messaggio al thread cliccando su enter

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 //cambio icona quando clicco dentro l'input per inviare il messaggio
 
 $('#send-msg').focus(function(){
-    //cambio l'icona presente -- il microfono
-    $('.footer #footer-icons i').removeClass('fa-microphone');
-    //faccio apparire l'altra icona
-    $('.footer #footer-icons i').addClass('fa-paper-plane');
+    //toggle entre le due classi di icona microfono e airplane
+    $('.footer #footer-icons i').toggleClass('fa-microphone fa-paper-plane');
 });
 
 $('#send-msg').blur(function(){
-    //cambio l'icona presente -- il microfono
-    $('.footer #footer-icons i').removeClass('fa-paper-plane');
-    //faccio apparire l'altra icona
-    $('.footer #footer-icons i').addClass('fa-microphone');
+    $('.footer #footer-icons i').toggleClass('fa-paper-plane fa-microphone');
 });
 
 // Milestone 1.2
 //Aggiunta di un messaggio​: l’utente scrive un testo nella parte bassa e cliccando “invia” il testo viene aggiunto al thread sopra, come messaggio verde
+//invio messagio cliccando enter
+$('#send-msg').keypress(function(){
+    if (event.which == 13) {
+        sendMessage()
+    }
+});
 
+//invio messaggio cliccando sull'icona
 $('.footer #footer-icons i').click(function(){
-    // debugger;
-    //intercetto il messaggio dell'utente
+    sendMessage();
+});
+
+//funzione per inviare il msg dentro la chat.
+function sendMessage (){
     var msgUser = $('#send-msg').val();
     console.log(msgUser);
     //primo stampo il messaggio dell'utente con text -- funzione che sostituisce tutto gia scritto prima.
@@ -41,9 +46,8 @@ $('.footer #footer-icons i').click(function(){
     $('#send-msg').val('');
 
     //milestone 2.1 -- dopo che l'utente ha inviato un messaggio: il pc invia una risposta dopo 1 secondo
-
     setTimeout(function(){
-        //
+        //messaggio del pc
         var answerPc = 'ok';
         $('.template .chat-with .chat-friend p:first-child').text(answerPc);
         //per l'orario corrente
@@ -54,10 +58,31 @@ $('.footer #footer-icons i').click(function(){
         $('.chat-with.real').append(pcTemplate);
         console.log(answerPc);
     }, 1000);
-
-});
-
+}
 
 //Milestone 2
-//2.1 Risposta dall’interlocutore: ​ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 //2.2 Ricerca utenti: ​scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+$('#search i').click(function(){
+    //recupero il testo scritto dall'utente, salvando un una variabile
+    var searchUser = $('#search input').val();
+    console.log(searchUser);
+    //vedo se il testo scritto dall'utente è presente in ogni p che ho
+    $('.messages').each(function(){
+        var nameMessage = $(this).text() ///text() vuoto legge solo la stringa
+        if (searchUser == nameMessage) {
+            $(this).siblings().show();
+        } else {
+            $('.messages .name').hide();
+
+        }
+    });
+    // $('.messages .last-message').each(function(){
+    //     var lastMessage = $(this).text() ///text() vuoto legge solo la stringa
+    //     if (searchUser == lastMessage) {
+    //         lastMessage.show();
+    //     } else {
+    //         $(this).hide();
+    //     }
+    // });
+
+});
