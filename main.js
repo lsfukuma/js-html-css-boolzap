520ec1f3f2041b8e699e2d1cc307859353597469
Leticia <lsf@protonmail.ch>
2020-05-12 09:47:15 +0200

    se il messaggio è vuoto, non è inviato

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $('.fa-paper-plane').click(function(){
     //intercetto il messaggio dell'utente
     var msgUser = $('#send-msg').val();
     console.log(msgUser);
-    //primo stampo il messaggio dell'utente con text --che sostituisce tutto gia scritto prima
+    //primo stampo il messaggio dell'utente con text -- funzione che sostituisce tutto gia scritto prima.
     $('.template .chat-with .chat-me p:first-child').text(msgUser);
     //per l'orario corrente
     var today = new Date();
@@ -16,8 +16,12 @@ $('.fa-paper-plane').click(function(){
     //copio l'elemento template
     var msgTemplate = $('.template .chat-with .chat-me').clone();
     console.log(msgTemplate);
-    //aggiungere il messaggio alla chat
-    $('.chat-with.real').append(msgTemplate)
+    //controllo che il messaggio non sia vuoto
+    if (msgUser !== ('')) {
+        //se non è vuoto, aggiungo il messaggio alla chat
+        $('.chat-with.real').append(msgTemplate)
+    }
+    $('#send-msg').val('');    
 });
 
 //cambio icona quando clicco dentro l'input per inviare il messaggio
